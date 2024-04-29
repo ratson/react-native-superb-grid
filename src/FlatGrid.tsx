07da9fab45b16581cd8a8b78a953ba976c82eee2
Ratson <github@ratson.name>
2024-04-29 13:08:54 +0800

    feat: export generic props

--- src/FlatGrid.tsx
+++ src/FlatGrid.tsx
@@ -9,7 +9,7 @@ import { type CommonProps, generateStyles } from './utils';
 /**
  * React Native Super Grid Properties
  */
-interface FlatGridBaseProps<ItemType> extends FlatListProps<ItemType>, CommonProps<ItemType> {
+export interface FlatGridProps<ItemType> extends FlatListProps<ItemType>, CommonProps<ItemType> {
   /**
    * Items to be rendered. renderItem will be called with each item in this array.
    */
@@ -21,97 +21,91 @@ interface FlatGridBaseProps<ItemType> extends FlatListProps<ItemType>, CommonPro
   customFlatList?: typeof FlatList;
 }
 
-export type FlatGridProps<ItemType = unknown> = React.PropsWithoutRef<FlatGridBaseProps<ItemType>> & React.RefAttributes<FlatList<ItemType>>;
+const FlatGrid = forwardRef(<T,>(props: FlatGridProps<T>, ref: React.Ref<FlatList<T>>) => {
+  const {
+    style = {},
+    spacing = 10,
+    fixed = false,
+    data,
+    itemDimension = 120,
+    renderItem,
+    horizontal = false,
+    onLayout: _ = null,
+    staticDimension,
+    maxDimension,
+    additionalRowStyle: externalRowStyle,
+    itemContainerStyle,
+    keyExtractor: customKeyExtractor = null,
+    invertedRow = false,
+    maxItemsPerRow,
+    adjustGridToStyles = false,
+    customFlatList: FlatListComponent = FlatList,
+    onItemsPerRowChange,
+    ...restProps
+  } = props;
 
-const FlatGrid = memo<FlatGridProps>(
-  forwardRef((props, ref) => {
-    const {
-      style = {},
-      spacing = 10,
-      fixed = false,
-      data,
-      itemDimension = 120,
-      renderItem,
-      horizontal = false,
-      onLayout: _ = null,
-      staticDimension,
-      maxDimension,
-      additionalRowStyle: externalRowStyle,
-      itemContainerStyle,
-      keyExtractor: customKeyExtractor = null,
-      invertedRow = false,
-      maxItemsPerRow,
-      adjustGridToStyles = false,
-      customFlatList: FlatListComponent = FlatList,
-      onItemsPerRowChange,
-      ...restProps
-    } = props;
+  const { onLayout, totalDimension, itemsPerRow, containerDimension, fixedSpacing } = useDimensions(props);
 
-    const { onLayout, totalDimension, itemsPerRow, containerDimension, fixedSpacing } = useDimensions(props);
+  const { containerStyle, containerFullWidthStyle, rowStyle } = useMemo(
+    () =>
+      generateStyles({
+        horizontal,
+        itemDimension,
+        containerDimension,
+        spacing,
+        fixedSpacing,
+        fixed,
+        itemsPerRow,
+      }),
+    [horizontal, itemDimension, containerDimension, itemsPerRow, spacing, fixedSpacing, fixed],
+  );
 
-    const { containerStyle, containerFullWidthStyle, rowStyle } = useMemo(
-      () =>
-        generateStyles({
-          horizontal,
-          itemDimension,
-          containerDimension,
-          spacing,
-          fixedSpacing,
-          fixed,
+  const { rows, keyExtractor } = useRows({
+    data,
+    invertedRow,
+    itemsPerRow,
+    keyExtractor: customKeyExtractor,
+    onItemsPerRowChange,
+  });
+
+  const renderRow = useRenderRow({
+    renderItem,
+    spacing,
+    keyExtractor: customKeyExtractor,
+    externalRowStyle,
+    itemContainerStyle,
+    horizontal,
+    invertedRow,
+  });
+
+  return (
+    <FlatListComponent
+      data={rows}
+      ref={ref}
+      extraData={totalDimension}
+      renderItem={({ item, index }) =>
+        renderRow({
+          rowItems: item,
+          rowIndex: index,
+          isLastRow: index === rows.length - 1,
           itemsPerRow,
-        }),
-      [horizontal, itemDimension, containerDimension, itemsPerRow, spacing, fixedSpacing, fixed],
-    );
+          rowStyle,
+          containerStyle,
+          containerFullWidthStyle,
+        })
+      }
+      style={[
+        {
+          ...(horizontal ? { paddingLeft: spacing } : { paddingTop: spacing }),
+        },
+        style,
+      ]}
+      onLayout={onLayout}
+      keyExtractor={keyExtractor}
+      {...restProps}
+      horizontal={horizontal}
+    />
+  );
+});
 
-    const { rows, keyExtractor } = useRows({
-      data,
-      invertedRow,
-      itemsPerRow,
-      keyExtractor: customKeyExtractor,
-      onItemsPerRowChange,
-    });
-
-    const renderRow = useRenderRow({
-      renderItem,
-      spacing,
-      keyExtractor: customKeyExtractor,
-      externalRowStyle,
-      itemContainerStyle,
-      horizontal,
-      invertedRow,
-    });
-
-    return (
-      <FlatListComponent
-        data={rows}
-        ref={ref}
-        extraData={totalDimension}
-        renderItem={({ item, index }) =>
-          renderRow({
-            rowItems: item,
-            rowIndex: index,
-            isLastRow: index === rows.length - 1,
-            itemsPerRow,
-            rowStyle,
-            containerStyle,
-            containerFullWidthStyle,
-          })
-        }
-        style={[
-          {
-            ...(horizontal ? { paddingLeft: spacing } : { paddingTop: spacing }),
-          },
-          style,
-        ]}
-        onLayout={onLayout}
-        keyExtractor={keyExtractor}
-        {...restProps}
-        horizontal={horizontal}
-      />
-    );
-  }),
-);
-
-FlatGrid.displayName = 'FlatGrid';
-
-export default FlatGrid;
+export default memo(FlatGrid);
--- src/SimpleGrid.tsx
+++ src/SimpleGrid.tsx
@@ -7,7 +7,7 @@ import useRenderRow from './hooks/useRenderRow';
 import useRows from './hooks/useRows';
 import { generateStyles } from './utils';
 
-export type SimpleGridProps<ItemType = unknown> = React.PropsWithoutRef<
+export type SimpleGridProps<ItemType> = ViewProps &
   Pick<
     FlatGridProps<ItemType>,
     | 'data'
@@ -27,97 +27,90 @@ export type SimpleGridProps<ItemType = unknown> = React.PropsWithoutRef<
     | 'adjustGridToStyles'
     | 'onItemsPerRowChange'
     | 'renderItem'
-  >
-> &
-  React.RefAttributes<View> &
-  ViewProps;
+  >;
 
-const SimpleGrid = memo<SimpleGridProps>(
-  forwardRef((props, ref) => {
-    const {
-      style = {},
-      spacing = 10,
-      fixed = false,
-      data,
-      itemDimension = 120,
-      renderItem,
-      horizontal = false,
-      additionalRowStyle: externalRowStyle,
-      itemContainerStyle,
-      keyExtractor: customKeyExtractor = null,
-      invertedRow = false,
-      onItemsPerRowChange,
-      ...restProps
-    } = {
-      onLayout: null,
-      listKey: undefined,
-      adjustGridToStyles: false,
-      ...props,
-    };
+const SimpleGrid = forwardRef(<T,>(props: SimpleGridProps<T>, ref: React.Ref<View>) => {
+  const {
+    style = {},
+    spacing = 10,
+    fixed = false,
+    data,
+    itemDimension = 120,
+    renderItem,
+    horizontal = false,
+    additionalRowStyle: externalRowStyle,
+    itemContainerStyle,
+    keyExtractor: customKeyExtractor = null,
+    invertedRow = false,
+    onItemsPerRowChange,
+    ...restProps
+  } = {
+    onLayout: null,
+    listKey: undefined,
+    adjustGridToStyles: false,
+    ...props,
+  };
 
-    const { onLayout, itemsPerRow, containerDimension, fixedSpacing } = useDimensions(props);
+  const { onLayout, itemsPerRow, containerDimension, fixedSpacing } = useDimensions(props);
 
-    const { containerStyle, containerFullWidthStyle, rowStyle } = useMemo(
-      () =>
-        generateStyles({
-          horizontal,
-          itemDimension,
-          containerDimension,
-          spacing,
-          fixedSpacing,
-          fixed,
-          itemsPerRow,
-        }),
-      [horizontal, itemDimension, containerDimension, itemsPerRow, spacing, fixedSpacing, fixed],
-    );
+  const { containerStyle, containerFullWidthStyle, rowStyle } = useMemo(
+    () =>
+      generateStyles({
+        horizontal,
+        itemDimension,
+        containerDimension,
+        spacing,
+        fixedSpacing,
+        fixed,
+        itemsPerRow,
+      }),
+    [horizontal, itemDimension, containerDimension, itemsPerRow, spacing, fixedSpacing, fixed],
+  );
 
-    const { rows, keyExtractor } = useRows({
-      data,
-      invertedRow,
-      itemsPerRow,
-      keyExtractor: customKeyExtractor,
-      onItemsPerRowChange,
-    });
-    const renderRow = useRenderRow({
-      renderItem,
-      spacing,
-      keyExtractor: customKeyExtractor,
-      externalRowStyle,
-      itemContainerStyle,
-      horizontal,
-      invertedRow,
-    });
+  const { rows, keyExtractor } = useRows({
+    data,
+    invertedRow,
+    itemsPerRow,
+    keyExtractor: customKeyExtractor,
+    onItemsPerRowChange,
+  });
+  const renderRow = useRenderRow({
+    renderItem,
+    spacing,
+    keyExtractor: customKeyExtractor,
+    externalRowStyle,
+    itemContainerStyle,
+    horizontal,
+    invertedRow,
+  });
 
-    return (
-      <View
-        style={[
-          {
-            ...(horizontal ? { paddingLeft: spacing } : { paddingTop: spacing }),
-          },
-          style,
-        ]}
-        ref={ref}
-        {...restProps}
-      >
-        {rows.map((row, index) => (
-          <View key={keyExtractor(row, index)} onLayout={onLayout}>
-            {renderRow({
-              rowItems: row as never,
-              rowIndex: index,
-              isLastRow: index === rows.length - 1,
-              itemsPerRow,
-              rowStyle,
-              containerStyle,
-              containerFullWidthStyle,
-              separators: null,
-            })}
-          </View>
-        ))}
-      </View>
-    );
-  }),
-);
+  return (
+    <View
+      style={[
+        {
+          ...(horizontal ? { paddingLeft: spacing } : { paddingTop: spacing }),
+        },
+        style,
+      ]}
+      ref={ref}
+      {...restProps}
+    >
+      {rows.map((row, index) => (
+        <View key={keyExtractor(row, index)} onLayout={onLayout}>
+          {renderRow({
+            rowItems: row as never,
+            rowIndex: index,
+            isLastRow: index === rows.length - 1,
+            itemsPerRow,
+            rowStyle,
+            containerStyle,
+            containerFullWidthStyle,
+            separators: null,
+          })}
+        </View>
+      ))}
+    </View>
+  );
+});
 
-SimpleGrid.displayName = 'SimpleGrid';
-
-export default SimpleGrid;
+export default memo(SimpleGrid);
